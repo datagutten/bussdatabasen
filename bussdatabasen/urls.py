821d90296ea473dff7402822a0de765468b0eb0b
datagutten <datagutten@datagutten.net>
2021-04-26 20:16:50 +0200

    Add django-debug-toolbar

--- bussdatabasen/urls.py
+++ bussdatabasen/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
+import debug_toolbar
 
 urlpatterns = [
     path('', include('bussdatabase.urls')),
@@ -24,5 +25,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     # url(r'^accounts/', include('registration.backends.hmac.urls')),
     path('accounts/', include('allauth.urls')),
+    path('__debug__/', include(debug_toolbar.urls))
+]
 
 ] # + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
